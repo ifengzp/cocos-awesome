bfc9a5375c1e24829175394af834a7d4359601fc
ifengzp <ifengzp@163.com>
2020-03-29 23:52:28 +0800

    init

---
+++ assets/Script/Helloworld.ts
@@ -0,0 +1,16 @@
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class Helloworld extends cc.Component {
+
+    @property(cc.Label)
+    label: cc.Label = null;
+
+    @property
+    text: string = 'hello';
+
+    start () {
+        // init logic
+        this.label.string = this.text;
+    }
+}
