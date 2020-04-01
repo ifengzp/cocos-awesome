e07909e403778587f0856a0f5e3381a933732bc4
ifengzp <ifengzp@163.com>
2020-04-01 22:11:08 +0800

    修复无限滚动问题

--- assets/Scene/Infinite_bg_scroll/Infinite_bg_scroll.ts
+++ assets/Scene/Infinite_bg_scroll/Infinite_bg_scroll.ts
@@ -8,6 +8,12 @@ export default class Infinite_bg_scroll extends cc.Component {
   bg2: cc.Node = null;
 
   speed: number = 500;
+  onLoad() {
+    const viewSize = cc.view.getVisibleSize();
+    this.bg2.getComponent(cc.Widget).left = viewSize.width
+    this.bg2.getComponent(cc.Widget).right = -viewSize.width
+  }
+
   update(dt) {
     const temp = dt * this.speed;
     if (this.bg2.x - temp <= 0) {
